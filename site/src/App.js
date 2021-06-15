e05efd9bdc9edb81c7532ff9fa47e4301c08f0f5
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-06-15 21:29:17 +0300

    Add Main Indent

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import CardsGrid from "./components/CardsGrid";
 import PanelGrid from "./components/PanelGrid";
+import MainIndent from "./components/MainIndent";
 function App() {
   return (
     <div className="App">
+      <MainIndent />
       <CardsGrid />
       <PanelGrid />
     </div>
---
+++ site/src/components/MainIndent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MainIndent = () => {
+  return (
+    <div className="main-indent">
+        <div class="main-indent-title">
+            <h1>ЗАГЛАВИЕ</h1>
+        </div>
+        <div class="main-indent-title">
+            <h2>ВТОРО ЗАГЛАВИЕ</h2>
+        </div>
+        <div class="main-indent-title">
+            <button>&#11163; ВИЖ ПОВЕЧЕ &#11163;</button>
+        </div>
+    </div>
+  );
+};
+
+export default MainIndent;
