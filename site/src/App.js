4c20ccb2dd77e12405b187ecdd8ee69cc0b1d3c0
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-06-27 15:45:34 +0300

    Add Navbar

--- site/src/App.js
+++ site/src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import CardsGrid from "./components/CardsGrid";
 import PanelGrid from "./components/PanelGrid";
 import MainIndent from "./components/MainIndent";
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() { 
     return (
       <div className="App">
         <MainIndent />
+        <Navbar />
         <CardsGrid />
         <PanelGrid />
       </div>
---
+++ site/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="">HOME</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">DIVE IN</a></li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
