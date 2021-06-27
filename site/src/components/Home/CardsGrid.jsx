550840b217d038dc9bc75152886c9cde290f2a0e
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-06-27 17:28:16 +0300

    Update Site Layout

--- site/src/components/Home/CardsGrid.jsx
+++ site/src/components/Home/CardsGrid.jsx
@@ -3,10 +3,23 @@ import Card from "./Card";
 
 const CardsGrid = () => {
   return (
-    <div className="card-grid">
-      <Card />
-      <Card />
-      <Card />
+    <div>
+
+      <div class="cards-indent"></div>
+
+      <div className="card-back">
+        <div className="card-grid">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      <div className="cards-indent-reversed"></div>
+
     </div>
   );
 };
