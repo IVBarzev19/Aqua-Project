6143cfb6358fe8d33f6f8f493e6c4cb02fd17e09
PSStefanov19 <PSStefanov19@codingburgas.bg>
2021-06-28 22:18:23 +0300

    Create base for DiveIn page

---
+++ site/src/components/DiveIn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Depth from './DiveIn/Depth';
+class DiveIn extends Component {
+    render() { 
+        return ( 
+            <div>
+                <Depth />
+            </div>
+         );
+    }
+}
+ 
+export default DiveIn;
---
+++ site/src/components/DiveIn/Depth.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+class Depth extends Component {
+    render() { 
+        return ( 
+        <div class="depth-container">
+            <div>
+                <h1>0 Metres</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt in culpa qui officia 
+                    deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat
+                </p>
+            </div>
+            <img src="https://images.pexels.com/photos/4055576/pexels-photo-4055576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+        </div> 
+        );
+    }
+}
+ 
+export default Depth;
