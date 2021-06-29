a7ffb9323e34ddf78a1924e766997701e37e7a85
Иван Владимиров Бързев <IVBarzev19@codingburgas.bg>
2021-06-29 11:50:43 +0300

    Add styling to Not Found page

--- site/src/components/NotFound.jsx
+++ site/src/components/NotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import "./../CSS/not-found.css";
+import Gif404 from "./../images/404.gif";
 const NotFound = () => {
     return ( 
         <div>
-            Not found
+            <div className="not-found">
+                <div>
+                    <h1>Couldn't Find The Page You Are Looking For</h1>
+ 
+                    <img src={ Gif404 } alt="penguin"/>
+                </div>
+            </div>
+ 
+            <div className="not-found-end">
+                <h1>HERE'S A DANCING PENGUIN INSTEAD</h1>
+            </div>
         </div>
      );
 }
