516bc8be1bccb98b0f0fe5ec5d999078e407745d
Даниел Златков Георгиев <DZGeorgiev19@codingburgas.bg>
2021-06-29 08:44:06 +0300

    Update Team members

--- site/src/components/About/Team.jsx
+++ site/src/components/About/Team.jsx
@@ -24,21 +24,15 @@ const Form = () => {
             </div>
 
             <div>
-                <img src="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg" alt="Bill"/>
-                <h1>Elon Musk</h1>
-                <h2>Scrum Trainer</h2>
+                <img src="https://avatars.githubusercontent.com/u/63741178?v=4" alt="Bill"/>
+                <h1>Ivan Burzev</h1>
+                <h2>Back-End</h2>
             </div>
 
             <div>
                 <img src="https://pbs.twimg.com/profile_images/1303592136269590528/hPWdTMgc.jpg" alt="Bill"/>
                 <h1>Boyko Borisov</h1>
                 <h2>QA Engineer</h2>
-            </div>
-
-            <div>
-                <img src="https://i.pinimg.com/originals/61/54/a6/6154a67d1c7879d4613fa525bc6439ef.jpg" alt="Bill"/>
-                <h1>Stalin</h1>
-                <h2>Back-End</h2>
             </div>
         </div>
     </div>
